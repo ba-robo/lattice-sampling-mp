0d764ed81ae9eac477474291896d7cf550fcc727
wei zhao <wei.zhao@micro-i.com.cn>
2025-12-31 11:18:38 +0800

    refactor Lattices.cpp

--- Cpp/include/Lattices.h
+++ Cpp/include/Lattices.h
@@ -1,9 +1,7 @@
-//
+ //
 // Created by itai on 7/15/24.
 //
 
-// #ifndef LATTICES_H
-// #define LATTICES_H
 #pragma once
 
 #include <string>
@@ -11,8 +9,6 @@
 #include <tuple>
 #include <random>
 #include <thread>
-// #include <Eigen/src/Core/Matrix.h>
-// #include <Eigen/src/Core/functors/TernaryFunctors.h>
 #include <float.h>
 #include <map>
 #include <Eigen/Dense>
@@ -29,179 +25,73 @@ namespace Lattices {
     class Lattice {
     public:
         Lattice(int dim, double delta, double epsilon, LatticeType type);
-        void setBounds(std::vector<double> * bounds);
 
-                    class TVectorEquals {
-            public:
-                bool operator()(const Eigen::VectorXd& v1, const Eigen::VectorXd& v2) const {
-                    bool res = true;
-                    for (int i = 0; i < v1.size(); ++i) {
-                        res = res && std::fabs(v1[i] - v2[i]) < 0.0001;
-                    }
-                    return res;
-                }
-            };
+        void setBounds(std::vector<double> *bounds);
 
-            // Hash function for Eigen matrix and vector.
-            // The code is from `hash_combine` function of the Boost library. See
-            // http://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine .
-            template<typename T>
-            struct matrix_hash : std::unary_function<T, size_t> { // moo!!
-                std::size_t operator()(T const& matrix) const {
-                    // Note that it is oblivious to the storage order of Eigen matrix (column- or
-                    // row-major). It will give you the same hash value for two different matrices if they
-                    // are the transpose of each other in different storage order.
-                    size_t seed = 0;
-                    for (size_t i = 0; i < matrix.size(); ++i) {
-                        // auto elem = *(matrix.data() + i);
-                        // auto roundElem = roundf(*(matrix.data() + i) * 10000) / 10000;
-                        int roundElem = round(*(matrix.data() + i) * 10000);
-                        // int elem = (int)(10000 * roundElem);
-                        // std::cout << "elem:" << elem << ",";
-                        seed ^= std::hash<typename T::Scalar>()(roundElem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-                    }
-                    // std::cout << ",  ~~~seed=" << seed << std::endl;
-                    return seed;
-                }
-            };
+        std::vector<double> sample();
 
-            static double distEuclid(const Eigen::VectorXd& v1, const Eigen::VectorXd& v2, int d_) {
-                double res = 0;
-                for (int w = 0; w < d_; ++w) {
-                    res += std::pow(v1[w] - v2[w], 2);
-                }
-                return std::sqrt(res);
-            }
-
-            static bool compareDoubles(double a, double b) {
-                return fabs(a - b) < DBL_EPSILON;
-            }
-
-            static std::string EigenToString(const Eigen::VectorXd& v){
-                std::string res = "";
-                for (int i = 0; i < v.size(); ++i) {
-                    int valint = std::round(v(i) * pow(10, 6));
-                    double val = valint / pow(10, 6);
-                    res += std::to_string(val) + ",";
+        class TVectorEquals {
+        public:
+            bool operator()(const Eigen::VectorXd &v1, const Eigen::VectorXd &v2) const {
+                bool res = true;
+                for (int i = 0; i < v1.size(); ++i) {
+                    res = res && std::fabs(v1[i] - v2[i]) < 0.0001;
                 }
                 return res;
             }
+        };
 
+        // Hash function for Eigen matrix and vector.
+        // The code is from `hash_combine` function of the Boost library. See
+        // http://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine .
+        template<typename T>
+        struct matrix_hash : std::unary_function<T, size_t> { // moo!!
+            std::size_t operator()(T const &matrix) const {
+                // Note that it is oblivious to the storage order of Eigen matrix (column- or
+                // row-major). It will give you the same hash value for two different matrices if they
+                // are the transpose of each other in different storage order.
+                size_t seed = 0;
+                for (size_t i = 0; i < matrix.size(); ++i) {
+                    // auto elem = *(matrix.data() + i);
+                    // auto roundElem = roundf(*(matrix.data() + i) * 10000) / 10000;
+                    int roundElem = round(*(matrix.data() + i) * 10000);
+                    // int elem = (int)(10000 * roundElem);
+                    // std::cout << "elem:" << elem << ",";
+                    seed ^= std::hash<typename T::Scalar>()(roundElem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+                }
+                // std::cout << ",  ~~~seed=" << seed << std::endl;
+                return seed;
+            }
+        };
 
-        std::tuple<int, double, double> goOverSamples(int type = 0, double r = 1, bool createFile = false);
-        long getSamplesInBall(bool createFile = false, int type = 0);
+        static double distEuclid(const Eigen::VectorXd &v1, const Eigen::VectorXd &v2, int d_) {
+            double res = 0;
+            for (int w = 0; w < d_; ++w) {
+                res += std::pow(v1[w] - v2[w], 2);
+            }
+            return std::sqrt(res);
+        }
+
+        static bool compareDoubles(double a, double b) {
+            return fabs(a - b) < DBL_EPSILON;
+        }
 
         std::tuple<long, double> getSampleCountInBallNew();
 
-        long getSampleCountInBall();
     protected:
         int d_;
-        double delta_;
-        double epsilon_;
         double r_;
         double rescale_;
-        double max_ind_;
-        std::vector<double>* bounds_;
-        std::mutex mtx;
+        double max_ind_;  // max_ind_ 是晶格采样的最大索引范围，用于定义采样空间的边界
+        LatticeType type_;
         Eigen::MatrixXd T_;
-    };
-
-    class Zn : public Lattice {
-    public:
-        Zn(int dim, double delta, double epsilon) : Lattice(dim, delta, epsilon, LatticeType::Zn) {
-            rescale_ = (delta * epsilon) / std::sqrt(1 + pow(epsilon, 2));
-            // max_ind_ = ceil(r_ / rescale_);
-            max_ind_ = ceil(r_ / (2.0*rescale_));
-            // Zn rescaled generator
-            T_ = Eigen::MatrixXd(dim, dim);
-            for (int j = 0; j < dim; ++j) {
-                for (int w = 0; w < dim; ++w) {
-                    if (j == w) {
-                        T_(j, w) = rescale_;
-                    } else {
-                        T_(j, w) = 0;
-                    }
-                }
-            }
-            gen_ = std::mt19937{seed_()};
-            dist_ = std::uniform_int_distribution<>{0, max_ind_};
-        }
-
-        std::vector<double> sample();
-    private:
         std::random_device seed_;
-        std::mt19937 gen_; // seed the generator
-        std::uniform_int_distribution<> dist_; // set min and max
-        int max_ind_;
+        std::mt19937 gen_;
+        std::uniform_int_distribution<> dist_;
+        std::vector<double>* bounds_;
     };
-    //
-    // class DnStar : public Lattice {
-    // public:
-    //     DnStar(int dim, double delta, double epsilon) : Lattice(dim, delta, epsilon) {
-    //         double beta = (delta * epsilon) / std::sqrt(1 + pow(epsilon, 2));
-    //         if (dim % 2 == 0) {
-    //             rescale_ = sqrt(8.0 / dim) * beta;
-    //         } else {
-    //             rescale_ =  (4*beta) / sqrt(2.0 * dim - 1);
-    //         }
-    //         max_ind_ = ceil(sqrt(r_2_) / rescale_);
-    //         // DnStar rescaled generator
-    //         T_ = Eigen::MatrixXd(dim, dim);
-    //         for (int j = 0; j < dim; ++j) {
-    //             for (int w = 0; w < dim; ++w) {
-    //                 if (j < dim - 1) {
-    //                     if (j == w) {
-    //                         T_(j, w) = rescale_;
-    //                     } else {
-    //                         T_(j, w) = 0;
-    //                     }
-    //                 } else {
-    //                     T_(j, w) = rescale_ * 0.5;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     std::vector<double> sample();
-    // private:
-    //
-    // };
-    //
-    // class AnStar : public Lattice {
-    // public:
-    //     AnStar(int dim, double delta, double epsilon) : Lattice(dim, delta, epsilon) {
-    //         double beta = (delta * epsilon) / std::sqrt(1 + pow(epsilon, 2));
-    //         rescale_ = sqrt((12.0 * (dim + 1)) / (dim * (dim + 2))) * beta;
-    //         double cubeSizeFix = sqrt(12.0 / (dim + 2)) * beta;
-    //         max_ind_ = ceil(sqrt(r_2_) / cubeSizeFix);
-    //         // DnStar rescaled generator
-    //         T_ = Eigen::MatrixXd(dim, dim);
-    //         double anstar_x = 1.0 / (dim + 1 - sqrt(dim + 1));
-    //         for (int j = 0; j < dim; ++j) {
-    //             for (int w = 0; w < dim; ++w) {
-    //                 if (j == 0) {
-    //                     if (w < dim - 1) {
-    //                         T_(j, w) = rescale_;
-    //                     } else {
-    //                         T_(j, w) = rescale_ * (anstar_x - 1);
-    //                     }
-    //                 } else {
-    //                     if (w == j - 1) {
-    //                         T_(j, w) = -rescale_;
-    //                     } else if (w < dim - 1) {
-    //                         T_(j, w) = 0;
-    //                     } else {
-    //                         T_(j, w) = rescale_ * anstar_x;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     std::vector<double> sample();
-    // private:
-    //
-    // };
+
+
 
 }
 
--- Cpp/src/Lattices.cpp
+++ Cpp/src/Lattices.cpp
@@ -20,38 +20,37 @@
 using namespace std::chrono_literals;
 
 void getIntegerVectors(int max_val, int dim, std::vector<std::vector<double> *> &powerSetRes, int i) {
-    std::vector<std::vector<double>*> tmp;
+    std::vector<std::vector<double> *> tmp;
     bool start = false;
     if (powerSetRes.empty()) {
         powerSetRes.push_back(new std::vector<double>);
         start = true;
     }
-    for (std::vector<double>* set: powerSetRes) {
+    for (std::vector<double> *set: powerSetRes) {
         for (int j = 0; j < max_val; j++) {
-            auto* new_set = new std::vector<double>(*set);
+            auto *new_set = new std::vector<double>(*set);
             new_set->insert(new_set->begin(), j);
             tmp.push_back(new_set);
         }
     }
     if (start) powerSetRes.clear();
-    for (std::vector<double>* set: tmp) {
+    for (std::vector<double> *set: tmp) {
         if (set->size() == i) powerSetRes.push_back(set);
     }
     auto it = powerSetRes.begin();
-    while(it != powerSetRes.end()) {
-        if((*it)->size() < i) {
+    while (it != powerSetRes.end()) {
+        if ((*it)->size() < i) {
             auto toDelete = *it;
             it = powerSetRes.erase(it);
             delete toDelete;
-        }
-        else {
+        } else {
             ++it;
         }
     }
     if (i < dim) getIntegerVectors(max_val, dim, powerSetRes, i + 1);
 }
 
-static std::string toString(const Eigen::VectorXd& v){
+static std::string toString(const Eigen::VectorXd &v) {
     std::string res = "";
     for (int i = 0; i < v.size(); ++i) {
         int valint = std::round(v(i) * pow(10, 6));
@@ -65,7 +64,7 @@ bool areDoubleEq(double a, double b) {
     return fabs(a - b) < DBL_EPSILON;
 }
 
-std::vector<std::string>* getSampleLines(const std::string &bin) {
+std::vector<std::string> *getSampleLines(const std::string &bin) {
     // Open the binary file for reading
     std::ifstream inputFile(bin, std::ios::binary);
 
@@ -75,7 +74,7 @@ std::vector<std::string>* getSampleLines(const std::string &bin) {
     }
 
     // Vector to store the lines as strings
-    auto* lines = new std::vector<std::string>;
+    auto *lines = new std::vector<std::string>;
 
     // Buffer to store each "line"
     std::string currentLine;
@@ -111,7 +110,7 @@ struct LatticeNN {
 
 template<typename T>
 struct matrix_hashI : std::unary_function<T, size_t> {
-    std::size_t operator()(T const& matrix) const {
+    std::size_t operator()(T const &matrix) const {
         // Note that it is oblivious to the storage order of Eigen matrix (column- or
         // row-major). It will give you the same hash value for two different matrices if they
         // are the transpose of each other in different storage order.
@@ -131,7 +130,7 @@ struct matrix_hashI : std::unary_function<T, size_t> {
 
 class TVectorEqualsI {
 public:
-    bool operator()(const Eigen::VectorXi& v1, const Eigen::VectorXi& v2) const {
+    bool operator()(const Eigen::VectorXi &v1, const Eigen::VectorXi &v2) const {
         for (int i = 0; i < v1.size(); ++i) {
             if (v1[i] != v2[i]) return false;
         }
@@ -168,7 +167,7 @@ std::tuple<long, double> Lattices::Lattice::getSampleCountInBallNew() {
                 Eigen::VectorXd newNeighbor = sample + sign * T_.col(i);
                 Eigen::VectorXi newNeighborI = sampleI;
                 newNeighborI[i] += sign;
-                if (visited.contains(newNeighborI) && visited[newNeighborI] < 2*d_) {
+                if (visited.contains(newNeighborI) && visited[newNeighborI] < 2 * d_) {
                     visited[newNeighborI]++;
                     if (visited[newNeighborI] == 2 * d_)
                         visited.erase(newNeighborI);
@@ -181,7 +180,7 @@ std::tuple<long, double> Lattices::Lattice::getSampleCountInBallNew() {
                         samples++;
                         sumOfEdges += newvNorm;
                         openTemp.push_back(
-                            {newNeighbor, newNeighborI});
+                                {newNeighbor, newNeighborI});
                     }
                     // if (newvNorm < 1.5 * r_ || compareDoubles(newvNorm, 1.5 * r_)) {
                     //     openTemp.push_back(
@@ -195,58 +194,8 @@ std::tuple<long, double> Lattices::Lattice::getSampleCountInBallNew() {
     return {samples, sumOfEdges};
 }
 
-long Lattices::Lattice::getSampleCountInBall() {
-    int d_ = this->d_;
-    std::vector<Eigen::VectorXd> open;
-    std::vector<Eigen::VectorXd> openTemp;
-    std::unordered_map<Eigen::VectorXd, int, matrix_hash<Eigen::VectorXd>, TVectorEquals> visited;
-    long samples = 1;
-    Eigen::VectorXd root = Eigen::VectorXd::Zero(d_);
-    // add root
-    visited[root] = 0;
-    open.push_back(root);
-    std::vector<Eigen::VectorXd> result;
-    Eigen::VectorXd bla;
-    while (!open.empty() || !openTemp.empty()) {
-        if (open.empty()) {
-            // we have the next wave of neighbors
-            open = openTemp;
-            openTemp.clear();
-        }
-        Eigen::VectorXd v = open[open.size() - 1];
-        open.pop_back();
-        int sign = -1;
-        for (int j = 0; j < 2; ++j) {
-            sign = -sign;
-            for (int i = 0; i < d_; ++i) {
-                Eigen::VectorXd newNeighbor = v + sign * T_.col(i);
-                std::string newNeighStr = EigenToString(newNeighbor);
-
-                if (visited.contains(newNeighbor) && visited[newNeighbor] < 2*d_) {
-                    visited[newNeighbor]++;
-                    if (visited[newNeighbor] == 2 * d_)
-                        visited.erase(newNeighbor);
-                } else {
-                    visited[newNeighbor] = 1;
-                    double newvNorm = 0;
-                    bool inArea = true;
-                    newvNorm = distEuclid(newNeighbor, root, d_);
-                    inArea = inArea &&  (newvNorm < r_ || compareDoubles(newvNorm, r_));
-                    if (inArea) {
-                        std::cout << EigenToString(newNeighbor) << std::endl;
-                        samples++;
-                        result.push_back(newNeighbor);
-                        openTemp.push_back(newNeighbor);
-                    }
-                }
-            }
-        }
-    }
-    return samples;
-}
-
-Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType type):
-    d_(dim), delta_(delta), epsilon_(epsilon), bounds_(nullptr), r_(1) {
+Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType type) :
+        d_(dim), type_(type) {
     double beta = (delta * epsilon) / std::sqrt(1 + pow(epsilon, 2));
     // r = (r == -1 ? 1 :
     r_ = (2 * delta * (epsilon + 1)) / std::sqrt(1 + pow(epsilon, 2));
@@ -257,7 +206,7 @@ Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType ty
             break;
         case LatticeType::Zn:
             rescale_ = (2 * beta) / sqrt(dim);
-            max_ind_ = ceil(2*r_ / rescale_);
+            max_ind_ = ceil(2 * r_ / rescale_); // 沿每个坐标轴，最多需要 ±max_ind_ 个晶格点就能覆盖半径为 r_ 的球
             // Zn rescaled generator
             T_ = Eigen::MatrixXd(dim, dim);
             for (int j = 0; j < dim; ++j) {
@@ -274,9 +223,9 @@ Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType ty
             if (dim % 2 == 0) {
                 rescale_ = sqrt(8.0 / dim) * beta;
             } else {
-                rescale_ =  (4*beta) / sqrt(2.0 * dim - 1);
+                rescale_ = (4 * beta) / sqrt(2.0 * dim - 1);
             }
-            max_ind_ = 3*ceil(2*r_ / rescale_);
+            max_ind_ = 3 * ceil(2 * r_ / rescale_);
             // DnStar rescaled generator
             T_ = Eigen::MatrixXd(dim, dim);
             for (int j = 0; j < dim; ++j) {
@@ -293,15 +242,15 @@ Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType ty
                 }
             }
             T_ = T_.transpose().eval();
-        // std::cout << T_ << std::endl;
+            // std::cout << T_ << std::endl;
             break;
         case LatticeType::AnStar:
             rescale_ = sqrt((12.0 * (dim + 1)) / (dim * (dim + 2))) * beta;
             // double cubeSizeFix = sqrt(12.0 / (dim + 2)) * beta;
             // max_ind_ = 6*ceil(sqrt(r_ * r_) / cubeSizeFix);
             double anstar_x = 1.0 / (dim + 1 - sqrt(dim + 1));
-            double fix = 1.0 / std::min(anstar_x, 1-anstar_x);
-            max_ind_ = ceil(2*fix * (r_ / rescale_));
+            double fix = 1.0 / std::min(anstar_x, 1 - anstar_x);  // 补偿因子，处理 A_n* 晶格的非正交性
+            max_ind_ = ceil(2 * fix * (r_ / rescale_));
             // DnStar rescaled generator
             T_ = Eigen::MatrixXd(dim, dim);
             for (int j = 0; j < dim; ++j) {
@@ -325,6 +274,11 @@ Lattices::Lattice::Lattice(int dim, double delta, double epsilon, LatticeType ty
             }
             break;
     }
+    // 初始化随机数生成器（对于 File 类型不需要）
+    if (type != LatticeType::File) {
+        gen_ = std::mt19937{seed_()};
+        dist_ = std::uniform_int_distribution<>{0, static_cast<int>(max_ind_)};
+    }
 }
 
 void Lattices::Lattice::setBounds(std::vector<double> *bounds) {
@@ -332,11 +286,25 @@ void Lattices::Lattice::setBounds(std::vector<double> *bounds) {
 }
 
 
-std::vector<double> Lattices::Zn::sample() {
-    std::vector<double> res;// = new std::vector<double>();
-    for (int i = 0; i < d_; i++) {
-        int val = dist_(gen_);
-        res.push_back(rescale_ * (val - (max_ind_ / 2)));
+std::vector<double> Lattices::Lattice::sample() {
+    if (type_ == LatticeType::Zn) {
+        // Zn 晶格：直接采样，无需矩阵变换
+        std::vector<double> res;
+        for (int i = 0; i < d_; i++) {
+            int val = dist_(gen_);  // val ∈ {0, 1, 2, ..., max_ind_}
+            // 使采样以原点为中心对称分布
+            res.push_back(rescale_ * (val - (max_ind_ / 2)));
+        }
+        return res;
+    } else if (type_ == LatticeType::DnStar || type_ == LatticeType::AnStar) {
+        // DnStar 和 AnStar 晶格：需要通过生成矩阵变换
+        Eigen::VectorXi random_ints(d_);
+        for (int i = 0; i < d_; ++i) {
+            random_ints(i) = dist_(gen_) - (max_ind_ / 2);
+        }
+        Eigen::VectorXd sample = T_ * random_ints.cast<double>();
+        std::vector<double> res(sample.data(), sample.data() + d_);
+        return res;
     }
-    return res;
+    return {};  // File 类型或其他情况
 }
